b93b7a04334010758b9203197f16716a27cf80d4
jobayer-rahman <jobayer_rahman@yahoo.com>
2023-05-05 11:14:12 +0600

    Database tables

--- gear_tracker/models.py
+++ gear_tracker/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+class Device(models.Model):
+    name = models.CharField(max_length=255)
+    device_type = models.CharField(max_length=255)
+    serial_number = models.CharField(max_length=255)
+    current_condition = models.CharField(max_length=255)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} {}".format(self.name, self.device_type)
+
+class Assignment(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    assigned_date = models.DateField()
+    return_date = models.DateField(null=True, blank=True)
+    given_condition = models.CharField(max_length=255)
+    return_condition = models.CharField(max_length=255)
